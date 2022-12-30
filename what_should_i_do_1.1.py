57b7588e4b2baab66a491ef789a3721d444c6c73
ella <lookisella@gmail.com>
2022-12-31 02:00:58 +1100

    test

---
+++ what_should_i_do_1.1.py
@@ -0,0 +1,23 @@
+import random
+
+def start():
+    print('Welcome')
+    n=''
+    while n.strip()=='':
+        n = input('What you wanna do? Please separated by space:')
+    lst = n.split(' ')
+    print(lst)
+    if len(lst) != 0:
+        try:
+            f = int(input(
+                'Is that correct? Please enter 1 if that is correct. Enter 2 if you wanna start again: '))
+            if f == 1:
+                do = random.choice(lst)
+                print(do)
+            elif f == 2:
+                print('Please enter again')
+                start()
+        except ValueError:
+            print('Wth was that, goodbye.')
+
+start()
