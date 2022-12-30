e6000e973075e18e63fd2ddb6c350ac9c22f84d4
ella <lookisella@gmail.com>
2022-12-31 02:21:30 +1100

    deleted the extra conditional statement

--- what_should_i_do.py
+++ what_should_i_do.py
@@ -2,22 +2,21 @@ import random
 
 def start():
     print('Welcome')
-    n=''
-    while n.strip()=='':
+    n = ''
+    while n.strip() == '':
         n = input('What you wanna do? Please separated by space:')
     lst = n.split(' ')
     print(lst)
-    if len(lst) != 0:
-        try:
-            f = int(input(
-                'Is that correct? Please enter 1 if that is correct. Enter 2 if you wanna start again: '))
-            if f == 1:
-                do = random.choice(lst)
-                print(do)
-            elif f == 2:
-                print('Please enter again')
-                start()
-        except ValueError:
-            print('Wth was that, goodbye.')
+    try:
+        f = int(input(
+            'Is that correct? Please enter 1 if that is correct. Enter 2 if you wanna start again: '))
+        if f == 1:
+            do = random.choice(lst)
+            print(do)
+        elif f == 2:
+            print('Please enter again')
+            start()
+    except ValueError:
+        print('Wth was that, goodbye.')
 
 start()
